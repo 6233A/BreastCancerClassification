926267e4e2aab89d60d0576e4f8b415e4131ee78
yz6a22 <114955698+yz6a22@users.noreply.github.com>
2023-01-04 16:12:33 +0000

    Update model_helper.py

--- model_helper.py
+++ model_helper.py
@@ -25,3 +25,37 @@ def get_testing_X_df(testing):
 # Returns the actual labels (Y - benign or malignant) for the test dataset, as a Dataframe
 def get_testing_Y_df(testing):
     return testing.loc[:, testing.columns == "diagnosis"]
+
+def evaluate_model(model, testing_X, testing_Y):
+    predictions = model.predict(testing_X)
+
+    CM = confusion_matrix(testing_Y, predictions)
+    TN = CM[0][0]
+    FN = CM[1][0]
+    TP = CM[1][1]
+    FP = CM[0][1]
+
+    # Precision
+    PRE = TP/(TP+FP)
+    # True positive rate or Recall
+    TPR = TP/(TP+FN)
+    # True negative rate
+    TNR = TN/(TN+FP)
+    # Talse positive rate
+    FPR = FP/(FP+TN)
+    # False negative rate
+    FNR = FN/(TP+FN)
+
+    accuracy = accuracy_score(testing_Y, predictions)
+    f1 = f1_score(testing_Y, predictions, average='binary')
+    print("Accuracy score: " + str(accuracy))
+    print("F1 Score: " + str(f1))
+    print("Precision: " + str(PRE))
+    print("True positive rate/Recall: " + str(TPR))
+    print("True negative rate: " + str(TNR))
+    print("False positive rate: " + str(FPR))
+    print("False negative rate: " + str(FNR))
+    print("True Positives: " + str(TP))
+    print("True Negatives: " + str(TN))
+    print("False Positives: " + str(FP))
+    print("False Negatives: " + str(FN))
