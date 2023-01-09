c567e37a3d265ff4f7452df4445d405504c4111d
Alphasaiz <sz2u22@soton.ac.uk>
2023-01-09 15:40:36 +0000

    Prepared dashboard for deployment

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,20 @@
 from explainerdashboard import ClassifierExplainer, ExplainerDashboard
 import model_helper
+from os.path import exists
 
-def run_dashboard(model):
+def save_dashboard(model):
     testing = model_helper.load_testing("testing.csv")
     testing_X = model_helper.get_testing_X_df(testing)
     testing_Y = model_helper.get_testing_Y_df(testing)
+
     explainer = ClassifierExplainer(model, testing_X, testing_Y)
-    ExplainerDashboard(explainer).run()
 
-def main():
+    db = ExplainerDashboard(explainer, title="Breast Cancer Classification - Gradient Boosting Dashboard", shap_interaction=False)
+    db.to_yaml("dashboard.yaml", explainerfile="explainer.joblib", dump_explainer=True)
+
+def create_app():
     gradientBoostingMachine = model_helper.load_model("gradientBoostingMachine.sav")
-    run_dashboard(gradientBoostingMachine)
-
-
-if __name__ == "__main__":
-    main()
+    save_dashboard(gradientBoostingMachine)
+    db = ExplainerDashboard.from_config("dashboard.yaml")
+    app = db.flask_server()
+    return app
